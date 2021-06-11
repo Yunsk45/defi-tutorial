92465b87ef364821e0b9557c1e03f01a7f5565c9
Nissim Zerbib <nissimzerbib@localhost.localdomain>
2021-06-11 16:33:10 +0200

    Deploying tokens and then Token farm. Send all token farm tokens inside farm contact.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,18 @@
 const TokenFarm = artifacts.require("TokenFarm");
+const DaiToken = artifacts.require("DaiToken");
+const DappToken = artifacts.require("DappToken");
 
-module.exports = function(deployer) {
-  deployer.deploy(TokenFarm);
+module.exports = function(deployer, network, accounts) {
+  
+  await deployer.deploy(DaiToken);
+  const daiToken = await DaiToken.deployed();
+
+  await deployer.deploy(DappToken);
+  const dappToken = await DappToken.deployed();
+
+  await deployer.deploy(TokenFarm, dappToken.address, daiToken.address);
+  const tokenFarm = await TokenFarm.deployed();
+
+  await dappToken.transfer(tokenFarm.address, '1000000000000000000000000');
+
 };
