613d2f290984f36c93b4619e86a2c5a814c3d1f3
Nissim Zerbib <nissimzerbib@localhost.localdomain>
2021-06-11 17:02:00 +0200

    Refactoring

--- test/TokenFarm.test.js
+++ test/TokenFarm.test.js
@@ -1,1 +1,32 @@
+const TokenFarm = artifacts.require("TokenFarm");
+const DaiToken = artifacts.require("DaiToken");
+const DappToken = artifacts.require("DappToken");
 
+
+require('chai')
+  .use(require('chai-as-promised'))
+  .should()
+
+
+contract('TokenFarm', accounts => {
+
+  let daiToken; 
+  let dappToken;
+  let tokenFarm;
+
+  before(async () => {
+    daiToken = await DaiToken.new();
+    dappToken = await DappToken.new();
+    tokenFarm = await TokenFarm.new(dappToken.address, daiToken.address);
+
+    await dappToken.transfer(tokenFarm.address, web3.utils.toWei("1000000", "ether"));
+
+  });
+  describe("Mock Dai deployment", async () => {
+    it("has a name", async () => {
+      const name = await daiToken.name();
+      assert.equal(name, 'Mock DAI Token');
+    });
+  });
+
+});
