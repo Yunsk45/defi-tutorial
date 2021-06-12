e18bccdd42294dcb80add43149485cf0645960b7
Yunsk45 <nisszer@gmail.com>
2021-06-12 12:19:28 +0200

    Refactoring React

---
+++ src/components/Main.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+/*
+import Navbar from './Navbar'
+import './App.css'
+
+import DaiToken from '../abis/DaiToken.json'
+import DappToken from '../abis/DappToken.json'
+import TokenFarm from '../abis/TokenFarm.json'
+const Web3 = require("web3");
+*/
+
+class Main extends Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, World!</h1>
+      </div>
+    );
+  }
+}
+
+export default Main;
